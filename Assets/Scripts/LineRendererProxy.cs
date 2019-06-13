249ed7db2e40c3dd43fc28c35305d04d3e0f8b1e
Chris Tchou <ctchou@unity3d.com>
2019-06-12 17:38:38 -0700

    The start

---
+++ Assets/Scripts/LineRendererProxy.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class LineRendererProxy : MonoBehaviour
+{
+    private Mesh theMesh;
+    public Material theMaterial;
+
+    private List<Matrix4x4> matrices;
+    private MaterialPropertyBlock properties;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (theMesh == null)
+        {
+            BuildMesh();
+        }
+
+        if (matrices == null)
+        {
+            matrices = new List<Matrix4x4>();
+        }
+
+        if (matrices.Count == 0)
+        {
+            matrices.Add(Matrix4x4.TRS(new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, Vector3.one));
+            matrices.Add(Matrix4x4.TRS(new Vector3(10.0f, 0.0f, 0.0f), Quaternion.identity, Vector3.one));
+            matrices.Add(Matrix4x4.TRS(new Vector3(0.0f, 10.0f, 0.0f), Quaternion.identity, Vector3.one));
+            matrices.Add(Matrix4x4.TRS(new Vector3(10.0f, 10.0f, 0.0f), Quaternion.identity, Vector3.one));
+            matrices.Add(Matrix4x4.TRS(new Vector3(0.0f, 0.0f, 20.0f), Quaternion.identity, Vector3.one));
+        }
+
+        if ((theMesh != null) && (theMaterial != null) && (matrices != null) && (matrices.Count > 0))
+            Graphics.DrawMeshInstanced(theMesh, 0, theMaterial, matrices, properties, ShadowCastingMode.Off, false, 0, null, LightProbeUsage.Off);
+    }
+
+    void OnWillRenderObject()
+    {
+        // Debug.Log(gameObject.name + " is being rendered by " + Camera.current.name + " at " + Time.time);
+    }
+
+    void BuildMesh()
+    {
+        theMesh = new Mesh();
+
+        theMesh.vertices = new Vector3[4]
+        {
+            new Vector3(0.0f, 0.0f, 0.0f),
+            new Vector3(0.0f, 1.0f, 0.0f),
+            new Vector3(1.0f, 0.0f, 0.0f),
+            new Vector3(1.0f, 1.0f, 0.0f),
+        };
+
+        theMesh.uv = new Vector2[4]
+        {
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.0f, 1.0f),
+            new Vector2(1.0f, 0.0f),
+            new Vector2(1.0f, 1.0f),
+        };
+
+        theMesh.triangles = new int[6]
+        {
+            0, 1, 2,
+            3, 2, 1,
+        };
+
+        theMesh.RecalculateBounds();
+    }
+}
---
+++ Assets/Shaders/UnlitLineShader.shader
@@ -0,0 +1,74 @@
+﻿Shader "Unlit/UnlitLineShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		_Color("Color", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+			// make fog work
+            #pragma multi_compile_fog
+			#pragma multi_compile_instancing
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                // UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+				UNITY_VERTEX_INPUT_INSTANCE_ID // necessary only if you want to access instanced properties in fragment Shader.
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+			UNITY_INSTANCING_BUFFER_START(Props)
+				UNITY_DEFINE_INSTANCED_PROP(fixed4, _Color)
+			UNITY_INSTANCING_BUFFER_END(Props)
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+				UNITY_SETUP_INSTANCE_ID(v);
+				UNITY_TRANSFER_INSTANCE_ID(v, o); // necessary only if you want to access instanced properties in the fragment Shader.
+
+                o.vertex = UnityObjectToClipPos(v.vertex);
+
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                // UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				UNITY_SETUP_INSTANCE_ID(i); // necessary only if any instanced properties are going to be accessed in the fragment Shader.
+
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv) * UNITY_ACCESS_INSTANCED_PROP(Props, _Color);
+                // apply fog
+                // UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
